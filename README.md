0debc4c0a7d8f22acb157829efbbf802a9a534c0
Daniel Farkas <farkas@vshosting.cz>
2022-12-04 23:44:34 +0100

    Complete refactor

--- README.md
+++ README.md
@@ -1,30 +1,29 @@
 # wedos-hook
 `wedos-hook` is a shell script to integrate the [dehydrated ACME client](https://github.com/lukas2511/dehydrated) with Wedos DNS servers for dns-01 challenges. 
 
-Connect the Wedos JSON WAPI with Let's Encrypt ACME dns-01 challenge via Dehydrated client to get certificate. Easy, using only bash shell and not a lot external dependecies.
+Connect the Wedos JSON WAPI with Let's Encrypt ACME dns-01 challenge via Dehydrated client to get certificate. Easy, using only bash shell and with few external dependecies.
 
-Hook is configured for localy deploying certificates to $BASEDIR/certs, if you want to deploy certificates with rsync,ssh or another way, just edit `deploy_cert` function.
+Hook is configured for localy deploying certificates to $BASEDIR/certs, if you want to use your own deploy hook, add `WAPI_DEPLOY_CERT_HOOK="path/to/hook.sh"` to dehydrated config. You can configure exit_hook as well: `WAPI_EXIT_HOOK="path/to/exit_hook.sh"`
 
 ## Requirements
-- [Wedos](https://hosting.wedos.com/) account with configured WAPI (more in Configuration)
-- [dehydrated ACME client script](https://github.com/lukas2511/dehydrated)
+- [Wedos](https://wedos.com/) account with configured WAPI (more in Configuration)
+- [dehydrated ACME client script]([https://github.com/lukas2511/dehydrated](https://github.com/dehydrated-io/dehydrated))
 - [jq](https://stedolan.github.io/jq/)
 - curl
 - grep
 - dig
 
 ## Configuration
-Download and [configure dehydrated per the documentation](https://github.com/lukas2511/dehydrated/blob/master/README.md#getting-started). And add to the config file following options:
+Login to the wedos Customer administration, open Customer tab and click to WAPI interface. Activate WAPI, setup allowed IP adresses, choose password and save. Preferred protocol has to be JSON !
+
+Download and [configure dehydrated per the documentation](https://github.com/dehydrated-io/dehydrated/blob/master/README.md#getting-started). And add to the config file following options:
 - `CHALLENGETYPE="dns-01"`
 - `HOOK="${BASEDIR}/wedos-hook.sh"`
+- `WAPI_LOGIN="email"`
+- `WAPI_PASS="xxxxxxxxxx"`
+- `WAPI_URL="https://api.wedos.com/wapi/json" (optional)`
+- `WAPI_WAIT=600 (optional, default 600)`
 
-Login to the wedos Customer administration, open Customer tab and click to WAPI interface. Activate WAPI, setup allowed IP adresses and choose password, then save. Preferred protocol has to be JSON !
-
-Download `wedos-hook` and place it in the same location as dehydrated. Open the script and change -
-- `login="EMAIL LOGIN"`
-- `wpass="P4SSW0RD"`
+Download `wedos-hook.sh`, place it in the same location as dehydrated and make it executable.
 
 That's all, finally you can run `dehydrated -c -f /etc/dehydrated/config`, add it to the crontab and get Let's Encrypt certificate with dns-01 challenge.
-
-## JSON ?
-Yes, I know. Method used in the script to send "JSON" request to wedos WAPI is not a JSON. But thats not my fault, and it is Wedos's problem. If they said "send us a json request" it means - "Send us a plaintext request, where you fill a json to the 'request' variable". :--)) Hosting masters.
--- wedos-hook.sh
+++ wedos-hook.sh
@@ -1,140 +1,272 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+# Derived from Silke Hofstra's PDNS hook 
+# https://github.com/silkeh/pdns_api.sh
+#
+# Daniel Farkas - 2022 
+# https://github.com/mrhackcz/wedos-hook
+#
+# Licensed under the EUPL
+#
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/collection/eupl
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+#
 
 set -f
+set -e
+set -u
+set -o pipefail
 
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+#DEBUG=1
 
-# Variables
-#~~~~~~~~~~~~~~~~~~~
-login="EMAIL LOGIN"
-wpass="P4SSW0RD"
+# Local directory
+DIR="$(dirname "$0")"
 
-hour=$(date +"%H")
-wpass_hash=$(echo -n $wpass|sha1sum|awk '{print $1}')
-wapi_url="https://api.wedos.com/wapi/json"
-authHash=$(echo -n $login$wpass_hash$hour|sha1sum|awk '{print $1}')
+# Config directories
+CONFIG_DIRS="/etc/dehydrated /usr/local/etc/dehydrated"
 
-# Arguments
-#~~~~~~~~~~~~~~~~~~~
-OPERATION=${1}
-FULL_DOMAIN=${2}
-CHALLENGE=${4}
+# Error handling
+error() { echo -e "\033[0;31mError: $*\033[0m" >&2; }
+warn() { echo -e "\033[0;33mWarning: $*\033[0m" >&2; }
+fatalerror() { error "$*"; exit 1; }
 
-# Functions
-#~~~~~~~~~~~~~~~~~~
+# Debug message
+debug() { [[ -z "${DEBUG:-}" ]] || echo -e "\033[0;36m$@\033[0m" >&2; }
+debugn() { [[ -z "${DEBUG:-}" ]] || echo -e "\033[0;36m$@\033[0m\n" >&2; }
 
-# Args - Not needed
-list_domains () {
-    jsonout=$(curl -ks ${wapi_url} --data-urlencode 'request={ "request": { "user": "'${login}'", "auth": "'${authHash}'", "command": "dns-domains-list" } }' )
-    echo ${jsonout} | jq -r "."
+# Load the configuration and set default values
+load_config () {
+  # Check for config in various locations
+  # From letsencrypt.sh
+  if [[ -z "${CONFIG:-}" ]]; then
+    for check_config in ${CONFIG_DIRS} "${PWD}" "${DIR}"; do
+      if [[ -f "${check_config}/config" ]]; then
+        CONFIG="${check_config}/config"
+        break
+      fi
+    done
+  fi
+
+  # Check if config was set
+  if [[ -z "${CONFIG:-}" ]]; then
+    # Warn about missing config
+    warn "No config file found, using default config!"
+  elif [[ -f "${CONFIG}" ]]; then
+    # shellcheck disable=SC1090
+    . "${CONFIG}"
+  fi
+
+  if [[ -n "${CONFIG_D:-}" ]]; then
+    if [[ ! -d "${CONFIG_D}" ]]; then
+      fatalerror "The path ${CONFIG_D} specified for CONFIG_D does not point to a directory."
+    fi
+
+    # Allow globbing
+    if [[ -n "${ZSH_VERSION:-}" ]]
+    then
+      set +o noglob
+    else
+      set +f
+    fi
+
+    for check_config_d in "${CONFIG_D}"/*.sh; do
+      if [[ -f "${check_config_d}" ]] && [[ -r "${check_config_d}" ]]; then
+        echo "# INFO: Using additional config file ${check_config_d}"
+        # shellcheck disable=SC1090
+        . "${check_config_d}"
+      else
+        fatalerror "Specified additional config ${check_config_d} is not readable or not a file at all."
+      fi
+    done
+
+    # Disable globbing
+    if [[ -n "${ZSH_VERSION:-}" ]]
+    then
+      set -o noglob
+    else
+      set -f
+    fi
+  fi
+
+  # Check required settings
+  [[ -n "${WAPI_LOGIN:-}" ]] || fatalerror "WAPI_LOGIN setting is required."
+  [[ -n "${WAPI_PASS:-}" ]]  || fatalerror "WAPI_PASS setting is required."
+
+  # Check optional settings
+  [[ -n "${WAPI_URL:-}" ]] || WAPI_URL="https://api.wedos.com/wapi/json"
+  [[ -n "${WAPI_WAIT:-}" ]] || WAPI_WAIT="600"
 }
 
-# Args - 1 - domain
-list_resources () {
-    jsonout=$(curl -ks ${wapi_url} --data-urlencode 'request={ "request": { "user": "'${login}'", "auth": "'${authHash}'", "command": "dns-rows-list", "data": { "domain": "'${1}'" } } }' )
-    echo ${jsonout} | jq -r "."
-   }
-
-# Args - 1 - domain | 2 - resource_id | 3 - rdata
-update_resource_target () {
-    jsonout=$(curl -ks ${wapi_url} --data-urlencode 'request={ "request": { "user": "'${login}'", "auth": "'${authHash}'", "command": "dns-row-update", "data": { "domain": "'${1}'", "row_id": "'${2}'", "ttl": "300", "rdata": "'${3}'" } } }' )
-    echo ${jsonout} | jq -r "."
+# Generate hash for every request
+build_hash () {
+	hour=$(date +"%H")
+	wpass_hash=$(echo -n $WAPI_PASS|sha1sum|head -c 40)
+	echo -n $WAPI_LOGIN$wpass_hash$hour|sha1sum|head -c 40
 }
 
-# Args - 1 - domain | 2 - name | 3 - rdata
-create_resource_target () {
-    jsonout=$(curl -ks ${wapi_url} --data-urlencode 'request={ "request": { "user": "'${login}'", "auth": "'${authHash}'", "command": "dns-row-add", "data": { "domain": "'${1}'", "name": "'${2}'", "ttl": "300", "type": "TXT", "rdata": "'${3}'", "auth_comment": "Edited by dehydrated WAPI hook" } } }' )
-    echo ${jsonout} | jq -r "."
+# Get all dns txt records contains ${2}
+get_token_id () {
+    jsonout=$(request "dns-rows-list" '{"domain":"'${1}'"}')
+    echo ${jsonout} | jq -r ".response.data.row[] | select(.rdtype==\"TXT\") | select (.rdata==\"${2}\") | .ID"
 }
 
-# Args - 1 - domain | 2 - resource_id
-delete_resource_target () {
-    jsonout=$(curl -ks ${wapi_url} --data-urlencode 'request={ "request": { "user": "'${login}'", "auth": "'${authHash}'", "command": "dns-row-delete", "data": { "domain": "'${1}'", "row_id": "'${2}'" } } }' )
-    echo ${jsonout} | jq -r "."
+# Send request to WAPI
+request () {
+    error=false
+
+    # Debug output
+    debug "# REQUEST"
+    debug "command: ${1}"
+    debug "data: ${2}"
+
+    if ! res=$(curl -4 -sSfL --stderr - ${WAPI_URL} --data-urlencode 'request={ "request": { "user": "'${WAPI_LOGIN}'", "auth": "'$(build_hash)'", "command": "'${1}'", "data":'${2}'} }'); then
+        error=true
+    fi
+
+    debugn "response: ${res}"
+
+    # Abort on failed request
+    if [[ "${res}" = *"error"* ]] || [[ "${error}" = true ]]; then
+        fatalerror "API error: ${res}"
+    fi
+
+    # Send data back if command is dns-rows-list
+    if [[ "${1}" == "dns-rows-list" ]]; then 
+        echo "${res}"
+    fi
 }
 
-commit_changes () {
-    jsonout=$(curl -ks ${wapi_url} --data-urlencode 'request={ "request": { "user": "'${login}'", "auth": "'${authHash}'", "command": "dns-domain-commit", "data": { "name": "'${1}'" } } }' )
+# Add dns record
+dns_row_add () {
+    request "dns-row-add" '{"domain":"'${1}'","name":"'${2}'","ttl":"300","type":"TXT","rdata":"'${3}'","auth_comment":"Edited_by_dehydrated_WAPI_hook"}'
 }
 
-process_domain() {
-	TLD=${FULL_DOMAIN##*.}
-	DOMAIN=${FULL_DOMAIN%%.$TLD*}
-	DOMAIN=${DOMAIN##*.}
-	DOMAIN=$DOMAIN.$TLD
-	SUBDOMAIN=${FULL_DOMAIN%%$DOMAIN*}
-	RESOURCE="_acme-challenge.${SUBDOMAIN}"
-	RESOURCE=${RESOURCE%.*}
-    DOMAIN_ACTIVE=`list_domains | jq -r ".response.data.domain[] | select(.name==\"${DOMAIN}\").status"`
-    RESOURCE_ID=`list_resources ${DOMAIN} | jq -r ".response.data.row[] | select(.rdtype==\"TXT\") | select (.name==\"${RESOURCE}\") | .ID"`
+# Delete dns record
+dns_row_delete () {
+    tokens=($(get_token_id "${1}" "${2}"))
+
+    for row_id in ${tokens[@]}; do
+        request "dns-row-delete" '{"domain":"'${1}'","row_id":"'${row_id}'"}'
+    done
 }
 
-verify_change() {
-	RESULT=""
-	until [ ".$RESULT" != "." ]
-	do
-		RESULT=`dig TXT $1 +noall +answer | grep IN | grep \"$2\"$`
-		sleep 60
-	done
+# Commit dns changes
+dns_domain_commit () {
+    request "dns-domain-commit" '{"name":"'${1}'"}'
 }
 
-deploy_challenge() {
-	process_domain
-	if [ "${DOMAIN_ACTIVE}" == "active" ]; then
-		if [ ".$RESOURCE_ID" != "." ]; then
-            update_resource_target $DOMAIN $RESOURCE_ID $CHALLENGE
-            commit_changes $DOMAIN
-		else
-			create_resource_target $DOMAIN $RESOURCE $CHALLENGE
-            commit_changes $DOMAIN
-		fi
-		verify_change $RESOURCE.$DOMAIN $CHALLENGE
-	else
-		echo "Domain is not active."
-	fi
-    commit_changes $DOMAIN
+# Handle additional exit hook
+exit_hook () {
+  if [[ -n "${WAPI_EXIT_HOOK:-}" ]]; then
+    exec ${WAPI_EXIT_HOOK}
+  fi
 }
 
-generate_csr() {
-    local DOMAIN="${1}" CERTDIR="${2}" ALTNAMES="${3}"
+# Handle additional deploy_cert hook
+deploy_cert () {
+  if [[ -n "${WAPI_DEPLOY_CERT_HOOK:-}" ]]; then
+    exec ${WAPI_DEPLOY_CERT_HOOK}
+  fi
 }
 
-clean_challenge() {
-    process_domain
-    delete_resource_target ${DOMAIN} ${RESOURCE_ID}
-    commit_changes
+main () {
+    hook="${1}"
+
+    if [[ ! "${hook}" =~ ^(deploy_challenge|clean_challenge|exit_hook|deploy_cert)$ ]]; then
+        exit 0
+    fi
+
+    load_config
+
+    # Debug output
+    debug "# Main"
+    debug "Bash: ${BASH_VERSION}"
+    debug "Args: $*"
+    debugn "Hook: ${hook}"
+
+    # Interface for exit_hook
+    if [[ "${hook}" = "exit_hook" ]]; then
+        shift
+        exit_hook "$@"
+        exit 0
+    fi
+
+    # Interface for deploy_cert
+    if [[ "${hook}" = "deploy_cert" ]]; then
+        deploy_cert "$@"
+        exit 0
+    fi
+
+    declare -A domains
+    # Loop through arguments per 3 
+    for ((i=2; i<=$#; i=i+3)); do
+        t=$((i + 2))
+        _domain="${!i}"
+        _token="${!t}"
+
+        if [[ "${_domain}" == "*."* ]]; then
+        debug "Domain ${_domain} is a wildcard domain, ACME challenge will be for domain apex (${_domain:2})"
+        _domain="${_domain:2}"
+        fi
+
+        domains[${_domain}]="${_token} ${domains[${_domain}]:-}"
+    done
+
+    unique_domains=($(echo ${!domains[@]} | tr ' ' '\n'| awk -F'.' '{print $(NF-1)"."$NF}' | sort -u | tr '\n' ' '))
+
+    # Debug output
+    debug "# Unique domains in certificate: "
+    debugn "  ${unique_domains[@]}"
+
+    # Loop through unique domains
+    for domain in ${unique_domains[@]}; do
+
+        challenge_list=($(echo ${!domains[@]} | tr ' ' '\n' | grep -E "${domain}$" | tr '\n' ' '))
+
+        # Debug output
+        debug "## Processing domain: ${domain}"
+        debugn "## With challenges: ${challenge_list[@]}"
+
+        # Loop through individually challenges
+        for challenge in ${challenge_list[@]}; do
+
+            # Debug output
+            debugn "### Processing challenge: ${challenge}"
+
+            record=$(echo "_acme-challenge."${challenge} | sed "s|\.$domain||g")
+            for token in ${domains[${challenge}]}; do
+                # Deploy a token(s)
+                if [[ "${hook}" = "deploy_challenge" ]]; then dns_row_add ${domain} ${record} ${token}; fi
+                # Remove a token(s)
+                if [[ "${hook}" = "clean_challenge" ]]; then dns_row_delete ${domain} ${token}; fi
+            done
+        done
+        dns_domain_commit $domain
+    done
+
+    # Wedos DNS is slow AF and it lasts up to $TTL (300) than record are on authoritative servers.
+    # https://help.wedos.cz/otazka/rychlost-propagace-dns-zaznamu-na-autoritatvni-server/86560/
+    if [[ "${hook}" = "deploy_challenge" ]]; then
+
+	# Maybe next time... WDNS just doesnt work as it should
+    	# Check if DNS record is available after commit.
+        #name_server=$(dig +short +noall +answer -t ns ${domain} | head -n1)
+        #debugn "Looking for \"${token}\" in ${record}.${domain} against @${name_server} with timeout ${WAPI_WAIT} seconds"
+		#timeout ${WAPI_WAIT} bash -c -- 'until dig +short '${record}'.'${domain}' TXT @'${name_server}' | grep -q -- "'${token}'"; do sleep 5; done'
+
+        # Debug output
+        debugn "# Waiting for ${WAPI_WAIT} seconds"
+        sleep ${WAPI_WAIT}
+    fi
 }
 
-deploy_cert() {
-    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
-    echo "Deploying locally"
-}
-
-invalid_challenge() {
-    local DOMAIN="${1}" RESPONSE="${2}"
-	echo "Not yet implemented: invalid_challenge"
-}
-
-request_failure() {
-    local STATUSCODE="${1}" REASON="${2}" REQTYPE="${3}"
-	echo "Not yet implemented: request_failure"
-}
-
-startup_hook() {
-	true
-}
-
-exit_hook() {
-	true
-}
-
-unchanged_cert() {
-	local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
-}
-
-OPERATION="$1"; shift
-if [[ "${OPERATION}" =~ ^(deploy_challenge|clean_challenge|deploy_cert|unchanged_cert|invalid_challenge|request_failure|generate_csr|startup_hook|exit_hook)$ ]]; then
-    "$OPERATION" "$@"
-fi
-
-exit 0
+main "$@"
